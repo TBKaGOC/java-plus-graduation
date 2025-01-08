aaa2cea418da5b777a503797e6dd2d7e3e99fdbd
Ivan Hamitsev <hamitsev-ie@yandex.ru>
2025-01-08 12:33:05 +0300

    Debug Event

--- main-server/src/main/java/ru/practicum/event/repository/EventRepository.java
+++ main-server/src/main/java/ru/practicum/event/repository/EventRepository.java
@@ -24,22 +24,22 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query(value = "INSERT INTO locations () valuse (:lat, :lon)", nativeQuery = true)
     void saveLocation(Float lat, Float lon);
 
-    @Query("SELECT e FROM Event e " +
+    @Query(value = "SELECT e FROM Event e " +
             "WHERE e.initiator IN :users " +
             "AND e.state IN :states " +
             "AND e.category IN :categories " +
             "AND e.eventDate < :rangeStart " +
-            "LIMIT :size")
-    List<Event> findByParametersWithoutEnd(List<Long> users, List<String> states, List<Long> categories, String rangeStart, Integer size);
+            "LIMIT :limitSize", nativeQuery = true)
+    List<Event> findByParametersWithoutEnd(List<Long> users, List<String> states, List<Long> categories, String rangeStart, Integer limitSize);
 
-    @Query("SELECT e FROM Event e " +
+    @Query(value = "SELECT e FROM Event e " +
             "WHERE e.initiator IN :users " +
             "AND e.state IN :states " +
             "AND e.category IN :categories " +
             "AND e.eventDate < :rangeStart " +
             "AND e.eventDate > :rangeEnd " +
-            "LIMIT :size")
-    List<Event> findByParametersWithEnd(List<Long> users, List<String> states, List<Long> categories, String rangeStart, String rangeEnd, Integer size);
+            "LIMIT :limitSize", nativeQuery = true)
+    List<Event> findByParametersWithEnd(List<Long> users, List<String> states, List<Long> categories, String rangeStart, String rangeEnd, Integer limitSize);
 
     @Query("SELECT e FROM Event e WHERE e.initiator.id IN :users " +
             "AND e.state in :states " +
