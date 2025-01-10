ab009ba409c34fe41ddb967390eb5541c45f5505
Попов Никита Михайлович <popovn@reshenie-soft.ru>
2025-01-10 08:45:49 +0300

    Убрал не используемые импорты

--- main-server/src/main/java/ru/practicum/user/repository/UserRepository.java
+++ main-server/src/main/java/ru/practicum/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.practicum.user.repository;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
