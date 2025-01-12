2206be90ce0d75acecc012f3939406dfb3a7e3b0
Ivan Hamitsev <hamitsev-ie@yandex.ru>
2025-01-12 19:50:06 +0300

    Fix imports

--- main-server/src/main/java/ru/practicum/comment/dto/CommentDto.java
+++ main-server/src/main/java/ru/practicum/comment/dto/CommentDto.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
 
 import static ru.practicum.util.JsonFormatPattern.JSON_FORMAT_PATTERN_FOR_TIME;
 
--- main-server/src/main/java/ru/practicum/comment/model/Comment.java
+++ main-server/src/main/java/ru/practicum/comment/model/Comment.java
@@ -2,27 +2,16 @@ package ru.practicum.comment.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-
-import java.time.LocalDateTime;
-
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.event.model.Event;
 import ru.practicum.user.model.User;
+
+import java.time.LocalDateTime;
+
+import static ru.practicum.util.JsonFormatPattern.JSON_FORMAT_PATTERN_FOR_TIME;
 
 @Entity
 @Getter
@@ -56,5 +45,6 @@ public class Comment {
     boolean isInitiator;
 
     @Column(name = "created", nullable = false)
+    @JsonFormat(pattern = JSON_FORMAT_PATTERN_FOR_TIME)
     LocalDateTime created;
 }
